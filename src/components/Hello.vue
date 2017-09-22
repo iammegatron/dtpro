180726d3a84a9a63df9b9d26afe8e709a52c3759
Z <iammegatron@gmail.com>
2017-09-22 17:12:05 -0400

    b

---
+++ src/components/Hello.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <a href="" class="button is-primary">Primary</a>
+    <div class="block">
+      {{msg}}
+    </div>
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'test'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
