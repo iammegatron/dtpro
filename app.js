4bf205368a91da4512d6fd1b91da2737e8d1b8df
Z <iammegatron@gmail.com>
2017-09-29 20:08:05 -0400

    update

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+
+
+var git = require('simple-git')()
+
 //middleware
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -11,7 +15,25 @@ app.get('/', function (req, res) {
 })
 
 
+app.get('/update', (req, res) => {
+  resp = {}
+  resp.cur_ver = require('./package.json').version;
+  git.fetch({"--tags":null}).tags((e,tags)=>{
+        resp.latest_ver = tags.latest
+        res.status(200).json(resp);
+  })
+});
 
+
+app.get('/update/start', (req, res) => {
+  git.pull(function(err, update) {
+    console.log("pulled from ",err, update)
+    if(update && update.summary.changes) {
+      //require('child_process').exec('./restart_server.sh');
+      //process.exit(1);
+    }
+  });
+});
 
 
 
